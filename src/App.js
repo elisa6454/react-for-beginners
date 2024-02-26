6e78119f0d5d8fa051cd86365d56a069c7f0673a
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2024-02-26 00:26:20 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
   return (
     <div>
-      <h1 className={styles.title}>Welcome back to React!</h1>
-      <Button text={"Continue"} />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write yout to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
-
 export default App;
