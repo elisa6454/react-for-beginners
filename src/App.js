b283311509ba57bb2878c6468c2a471c91fdcd7e
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2024-02-26 16:33:33 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write yout to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(`https://api.publicapis.org/entries`)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
+
 export default App;
